cf650d1a76ec8e965ace37aec6b9c2889f1e0d17
alpgunsever <alpgunsever@gmail.com>
2017-10-10 00:09:44 +0200

    Plotted stacked bar plot for Pclass and Survived

--- Kaggle/Titanic/scripts/analysis.py
+++ Kaggle/Titanic/scripts/analysis.py
@@ -4,6 +4,8 @@ Created on Mon Oct  9 18:06:13 2017
 @author: Alp
 """
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Load Training Data
 filename = "C:/Users/Alp/Desktop/Data-Science-Portfolio/Kaggle/Titanic/data/train.csv"
@@ -12,5 +14,22 @@ train = pd.read_csv(filename)
 # Explore training dataset
 print(train.shape)
 print(list(train))
-train.head()
-train.describe()
+print(train.head())
+print(train.describe())
+
+# Passenger class in connection to survived
+ind = np.unique(train["Pclass"])
+class_num_survived = [sum(train[train["Pclass"]==i]["Survived"]) for i in ind]
+class_num_drowned = [len(train[train["Pclass"]==i]["Survived"]) - 
+                     sum(train[train["Pclass"]==i]["Survived"]) for i in ind]
+width = 0.35 
+
+p1 = plt.bar(ind, class_num_drowned, width)
+p2 = plt.bar(ind, class_num_survived, width, bottom = class_num_drowned)
+
+plt.xlabel('Passenger Class')
+plt.title('Survived Count by Passenger Class')
+plt.xticks(ind, ('P1', 'P2', 'P3'))
+plt.yticks(np.arange(0, 500, 100))
+plt.legend((p1[0], p2[0]), ('Drowned', 'Survived'))
+plt.show()
