929c713f9ba66b0382b7d20804755d5e086c92f7
alpgunsever <alpgunsever@gmail.com>
2017-10-10 15:03:39 +0200

    added function to plot stacked bar for categorical variables vs survived

--- Kaggle/Titanic/scripts/analysis.py
+++ Kaggle/Titanic/scripts/analysis.py
@@ -13,23 +13,30 @@ train = pd.read_csv(filename)
 
 # Explore training dataset
 print(train.shape)
+print(train.info())
 print(list(train))
 print(train.head())
 print(train.describe())
 
-# Passenger class in connection to survived
-ind = np.unique(train["Pclass"])
-class_num_survived = [sum(train[train["Pclass"]==i]["Survived"]) for i in ind]
-class_num_drowned = [len(train[train["Pclass"]==i]["Survived"]) - 
-                     sum(train[train["Pclass"]==i]["Survived"]) for i in ind]
-width = 0.35 
+# Create a function to plot stacked bar chart for survived vs other variables
+def plot_stacked_bar(dataset, varname):    
+    ind = np.unique(dataset[varname])
+    class_num_survived = [sum(dataset[dataset[varname]==i]["Survived"]) for i in ind]
+    class_num_drowned = [len(dataset[dataset[varname]==i]["Survived"]) - 
+                     sum(dataset[dataset[varname]==i]["Survived"]) for i in ind]
+    width = 0.35 
 
-p1 = plt.bar(ind, class_num_drowned, width)
-p2 = plt.bar(ind, class_num_survived, width, bottom = class_num_drowned)
+    p1 = plt.bar(ind, class_num_drowned, width)
+    p2 = plt.bar(ind, class_num_survived, width, bottom = class_num_drowned)
 
-plt.xlabel('Passenger Class')
-plt.title('Survived Count by Passenger Class')
-plt.xticks(ind, ('P1', 'P2', 'P3'))
-plt.yticks(np.arange(0, 500, 100))
-plt.legend((p1[0], p2[0]), ('Drowned', 'Survived'))
-plt.show()
+    plt.xlabel(str(varname))
+    plt.title("Survived Count by " + varname)
+    if varname == "Pclass":
+        plt.xticks(ind, ('P1', 'P2', 'P3'))
+    plt.yticks(np.arange(0, 500, 100))
+    plt.legend((p1[0], p2[0]), ('Drowned', 'Survived'))
+    plt.show()
+
+plot_stacked_bar(train, "Pclass")
+plot_stacked_bar(train, "SibSp")
+plot_stacked_bar(train, "Parch")
